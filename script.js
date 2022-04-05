c77bc963508e91b42441dabad5cd0cd69c89c4b3
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-04-05 22:05:36 +0200

    add behaviour to mobile menu also add JS linters

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const hamburgerBtn = document.getElementsByClassName("toggle-button")[0];
-const navLinks = document.getElementsByClassName("navbar-links")[0];
-const logo = document.getElementsByClassName("logo")[0];
-const mailIicon = document.getElementsByClassName("mail-icon")[0];
-const closeIcon = document.getElementsByClassName("closeicon")[0];
+const hamburgerBtn = document.getElementsByClassName('toggle-button')[0];
+const navLinks = document.getElementsByClassName('navbar-links')[0];
+const logo = document.getElementsByClassName('logo')[0];
+const mailIicon = document.getElementsByClassName('mail-icon')[0];
+const header = document.getElementById('header');
 
+const menuToggle = () => {
+  header.classList.toggle('header-mobile-menu-opened');
+  navLinks.classList.toggle('active-navbar');
+  logo.classList.toggle('inactive');
+  hamburgerBtn.classList.toggle('toggle-button-opened');
+  mailIicon.classList.toggle('inactive');
+};
 
-hamburgerBtn.addEventListener("click", () => {
-    console.log(hamburgerBtn);
-    navLinks.classList.toggle("active-navbar");
-    logo.classList.toggle("inactive");
-    hamburgerBtn.classList.toggle("inactive");
-    mailIicon.classList.toggle("inactive");
-    closeIcon.classList.toggle("active");
-});
+hamburgerBtn.addEventListener('click', menuToggle);
+
+navLinks.addEventListener('click', menuToggle);
