dd354e1b914994d2ed098a79abdf3ef391b74cad
= <=>
2022-04-05 03:21:13 -0700

    styling mobile menu

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const hamburgerBtn = document.getElementsByClassName('toggle-button')[0]
-const navLinks = document.getElementsByClassName('navbar-links')[0]
-console.log(hamburgerBtn)
-hamburgerBtn.addEventListener('click', () => {
-    console.log(hamburgerBtn)
-    navLinks.classList.toggle('active')
-})
+const hamburgerBtn = document.getElementsByClassName("toggle-button")[0];
+const navLinks = document.getElementsByClassName("navbar-links")[0];
+const logo = document.getElementsByClassName("logo")[0];
+const mailIicon = document.getElementsByClassName("mail-icon")[0];
+const closeIcon = document.getElementsByClassName("closeicon")[0];
+
+
+hamburgerBtn.addEventListener("click", () => {
+    console.log(hamburgerBtn);
+    navLinks.classList.toggle("active-navbar");
+    logo.classList.toggle("inactive");
+    hamburgerBtn.classList.toggle("inactive");
+    mailIicon.classList.toggle("inactive");
+    closeIcon.classList.toggle("active");
+});
